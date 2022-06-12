6d48072529b6efc463358932b62465f87ae8e2b8
Chadly Riedewald <no925.c@gmail.com>
2022-06-13 00:59:35 +0200

    created react starter template

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { Fragment } from 'react';
+import GlobalStyles from './globalStyles';
+
+const App = () => {
+    return (
+        <Fragment>
+            <GlobalStyles />
+        </Fragment>
+    );
+};
+
+export default App;
---
+++ src/constants.js
@@ -0,0 +1,13 @@
+export const COLORS = {};
+
+export const SPACING = {};
+
+export const FONT_SIZES = {};
+
+export const FONT_WEIGHTS = {};
+
+export const LINE_HEIGHTS = {};
+
+export const BORDER_RADIUS = {};
+
+export const ELEVATION = {};
